9fec9fcde3ca4bab2d34bc7b631dfafff77d0ba4
Lina Jamal <linahjamal89@gmail.com>
2020-05-20 05:48:22 +0300

    make database schema

---
+++ server/src/database/models/Todos.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const TodosSchema = new Schema({
+  userID: {
+    type: String,
+    required: true,
+  },
+
+  title: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+  description: {
+    type: String,
+  },
+  important: {
+    type: Boolean,
+    default: false,
+  },
+  done: {
+    type: Boolean,
+    default: false,
+  },
+  time: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = model("Todos", TodosSchema);
