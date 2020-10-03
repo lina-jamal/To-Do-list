b2ccb5b80f076e888e4f0200d2880c049586ba1b
lina-jamal <linahjamal89@gmail.com>
2020-10-03 23:19:36 +0300

    fix the name and add the userid in todo input

--- server/src/router.js
+++ server/src/router.js
@@ -3,18 +3,19 @@ const todo = require("./database/models/Todos");
 router.get("/", (req, res) => {
   res.send("hi");
 });
-// router.post("/todo", (req, res) => {
-//   const todo = new todo({
-//     title: req.body.title,
-//     description: req.body.description,
-//     important: req.body.important,
-//     done: req.body.done,
-//   });
-//   todo
-//     .save()
-//     .then((gg) => {
-//       res.json(gg);
-//     })
-//     .catch((err) => console.error(err));
-// });
+router.post("/todo", (req, res) => {
+  const { title, description, important, userID, done } = req.body;
+  const Todo = new todo({
+    title,
+    description,
+    important,
+    userID,
+    done,
+  });
+  Todo.save()
+    .then((gg) => {
+      res.json(gg);
+    })
+    .catch((err) => console.error(err));
+});
 module.exports = router;
